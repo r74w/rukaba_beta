d48a9a933e6864088c5edf5f35dacc82a57356a8
dmitrii <timjoww@gmail.com>
2012-07-05 23:30:05 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RukabaBeta::Application.config.secret_token = '4b5e1a58e8e50afb25af53c4bcd502c64c336a91bfd5d3f4e3bb1262ed6b0dc4e1efebd7016aad313f4333a2bc448d31979da32087867a4f2472b7331cddf2e5'
